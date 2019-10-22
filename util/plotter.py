75c501c5687a5f8d80d80a7f1d2329a99aa72c6c
kosz <kosz@kth.se>
2019-10-22 18:51:33 +0200

    Minor fix / comment

--- util/plotter.py
+++ util/plotter.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.preprocessing import normalize
 
+
 def visualize(representation):
+    # last_layer = rln.get_layer("Last", -1)
+    # representation = last_layer.get_weights()
     representation = representation.reshape((32, 72))
     representation = normalize(representation)
     plt.axis('off')
