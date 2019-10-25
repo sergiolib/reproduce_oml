Fix a problem with softmax not being applied

experiments/exp4_2/omniglot_model.py
@@ -164,36 +164,40 @@ def evaluate_classification_mrcl(training_data, testing_data, rln, tln, classifi
     test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
     train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
     print(f"Start training with lr: {classification_parameters['online_learning_rate']}")
-    for class_id in range(200):
+    for class_id in range(10):
         seen_classes = seen_classes + 1
+        #for m in range((seen_classes - 1) * 15, seen_classes * 15):
         with tf.GradientTape() as tape:
+            #loss, _ = compute_loss(x_training[m], y_training[m], rln, tln, classification_parameters)
             loss, _ = compute_loss(x_training[(seen_classes - 1) * 15:seen_classes * 15],
-                                   y_training[(seen_classes - 1) * 15:seen_classes * 15], rln, tln,
-                                   classification_parameters)
+                               y_training[(seen_classes - 1) * 15:seen_classes * 15], rln, tln,
+                               classification_parameters)
         gradient_tln = tape.gradient(loss, tln.trainable_variables)
         classification_parameters['online_optimizer'](
             learning_rate=classification_parameters['online_learning_rate']).apply_gradients(
             zip(gradient_tln, tln.trainable_variables))
 
-        x_training_all = x_training[:seen_classes * 5]
-        y_training_all = y_training[:seen_classes * 5]
+        x_training_all = x_training[:seen_classes * 15]
+        y_training_all = y_training[:seen_classes * 15]
 
         x_testing_all = x_testing[:seen_classes * 5]
         y_testing_all = y_testing[:seen_classes * 5]
 
         loss, output = compute_loss(x_training_all, y_training_all, rln, tln, classification_parameters)
-        train_accuracy(x_training_all, output)
+        after_softmax = tf.nn.softmax(output, axis=1)
+        train_accuracy(y_training_all, after_softmax)
 
         loss, output = compute_loss(x_testing_all, y_testing_all, rln, tln, classification_parameters)
-        test_accuracy(y_testing_all, output)
+        after_softmax = tf.nn.softmax(output, axis=1)
+        test_accuracy(y_testing_all, after_softmax)
 
         results.append({"number_of_classes_seen": seen_classes,
                         "test_accuracy": str(test_accuracy.result().numpy()),
                         "train_accuracy": str(train_accuracy.result().numpy())})
 
+        if (class_id+1) % 50 == 0:
+            print(f"Class {class_id}, test accuracy {test_accuracy.result()},  train accuracy {train_accuracy.result()}")
+
         train_accuracy.reset_states()
         test_accuracy.reset_states()
-
-        if (class_id+1) % 50 == 0:
-            print(f"Class {class_id}, loss {loss}, accuracy {test_accuracy.result()}")
     return results