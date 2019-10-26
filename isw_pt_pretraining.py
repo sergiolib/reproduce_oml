fcaed0cde1da714da0de2fb7b26b160cbb3d8123
Sergio Liberman Bronfman <sergiolib@gmail.com>
2019-10-26 19:44:47 +0200

    Learning rates fixed

--- isw_pt_pretraining.py
+++ isw_pt_pretraining.py
@@ -11,7 +11,7 @@ from datasets.synth_datasets import gen_sine_data, gen_tasks
 
 # Parse arguments
 argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("--learning_rate", nargs="+", type=float, default=[0.00003],
+argument_parser.add_argument("--learning_rate", nargs="+", type=float, default=[1e-6],
                              help="Learning rate")
 argument_parser.add_argument("--epochs", type=int, default=20000,
                              help="Number of epochs to pre train for")
@@ -25,8 +25,6 @@ argument_parser.add_argument("--repetitions", type=int, default=40,
                              help="Number of train repetitions for generating the data samples")
 argument_parser.add_argument("--save_models_every", type=int, default=100,
                              help="Amount of epochs to pass before saving models")
-argument_parser.add_argument("--batch_size", type=int, default=100,
-                             help="Batch size")
 argument_parser.add_argument("--check_val_every", type=int, default=1,
                              help="Amount of epochs to pass before checking validation loss")
 
@@ -44,7 +42,7 @@ x_val = tf.convert_to_tensor(x_val, dtype=tf.float32)
 y_val = tf.convert_to_tensor(y_val, dtype=tf.float32)
 
 # Main pre training loop
-p = PretrainingBaseline(tf.keras.losses.MeanSquaredError())
+p = PretrainingBaseline(tf.keras.losses.MeanSquaredError(), l2_lambda=0.00)
 loss = float("inf")
 # Create logs directories
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -83,16 +81,16 @@ for tln_layers, rln_layers, lr in gen:
             with train_summary_writer.as_default():
                 tf.summary.scalar("Validation Loss", val_loss, step=epoch)
 
-            if previous_val_loss < val_loss:
+            if previous_val_loss - val_loss < 1e-5:
                 val_loss_counts += 1
                 if val_loss_counts == 1:
                     p.save_model(f"final_lr{lr}_rln{rln_layers}_rln{tln_layers}")
-                elif val_loss_counts >= 300:
+                elif val_loss_counts >= 50:
                     break
             else:
                 previous_val_loss = val_loss
                 val_loss_counts = 0
 
-        if epoch % args.save_models_every == 0:
-            p.save_model(f"{epoch}_lr{lr}_rln{rln_layers}_tln{tln_layers}")
-
+        # if epoch % args.save_models_every == 0:
+        #     p.save_model(f"{epoch}_lr{lr}_rln{rln_layers}_tln{tln_layers}")
+        #
