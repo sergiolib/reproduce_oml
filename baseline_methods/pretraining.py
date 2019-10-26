Fixed bug with computation of loss

baseline_methods/pretraining.py
@@ -18,6 +18,7 @@ class PretrainingBaseline:
 
     def build_model(self, n_layers_rln=6, n_layers_tln=2, hidden_units_per_layer=300, one_hot_depth=10):
         self.model_rln, self.model_tln = mrcl_isw(n_layers_rln, n_layers_tln, hidden_units_per_layer, one_hot_depth)
+        self.compute_loss = tf.function(self._compute_loss)
 
     def save_model(self, name):
         try:
@@ -35,8 +36,7 @@ class PretrainingBaseline:
         self.model_rln = tf.keras.models.load_model(f"saved_models/{name}_rln.tf")
         self.model_tln = tf.keras.models.load_model(f"saved_models/{name}_tln.tf")
 
-    @tf.function
-    def compute_loss(self, x, y):
+    def _compute_loss(self, x, y):
         return self.loss_function(y, self.model_tln(self.model_rln(x)))
 
     def pre_train(self, x_pre_train, y_pre_train, optimizer, batch_size=1):

isw_pt_pretraining.py
@@ -46,7 +46,6 @@ y_val = tf.convert_to_tensor(y_val, dtype=tf.float32)
 # Main pre training loop
 p = PretrainingBaseline(tf.keras.losses.MeanSquaredError())
 loss = float("inf")
-pre_train_not_finished = True
 # Create logs directories
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 gen = product(list(range(1, 8)), args.learning_rate)
@@ -62,7 +61,7 @@ for rln_layers, lr in gen:
     previous_val_loss = float("inf")
     val_loss = float("inf")
     epoch = 0
-    while pre_train_not_finished:
+    while True:
         # Sample data
         x_train, y_train, _, _ = gen_sine_data(tasks=train_tasks, n_functions=args.n_functions,
                                                sample_length=args.sample_length,
@@ -79,7 +78,6 @@ for rln_layers, lr in gen:
         y_train = tf.reshape(y_train, (-1,))
 
         p.pre_train(x_train, y_train, optimizer, batch_size=args.batch_size)
-
 
         if epoch % args.check_val_every == 0:
             val_loss = p.compute_loss(x_val, y_val)