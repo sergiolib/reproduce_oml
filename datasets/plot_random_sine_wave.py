baeed5e6224879a9453b71a8051baaffab297de1
kosz <kosz@kth.se>
2019-10-27 18:41:42 +0100

    Add dataset sample figures

--- datasets/plot_random_sine_wave.py
+++ datasets/plot_random_sine_wave.py
@@ -1,9 +1,15 @@
-import synth_datasets
+from datasets.synth_datasets import gen_tasks, gen_sine_data
 import matplotlib.pyplot as plt
 
-sine_waves = synth_datasets.gen_sine_data(n_id=900, n_samples=320)
-partition = synth_datasets.partition_sine_data(sine_waves)
 
-plt.plot(partition["pretraining"]["z"][:320], partition["pretraining"]["y"][:320], "b.")
+tasks = gen_tasks(10)
+x_traj, y_traj, _, _ = gen_sine_data(tasks)
+
+
+plt.scatter(x_traj[0][0][:][:, 0], y_traj[0][0][:])
+plt.scatter(x_traj[1][0][:][:, 0], y_traj[1][0][:])
+# plt.scatter(x_traj[2][0][:][:, 0], y_traj[2][0][:])
+# plt.scatter(x_traj[3][0][:][:, 0], y_traj[3][0][:])
+# plt.scatter(x_traj[4][0][:][:, 0], y_traj[4][0][:])
 plt.grid()
-plt.savefig("sample.png")
+plt.show()
