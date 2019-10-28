f883c7ee8309b6df3837f9683702a1c6cdbf17a8
Sergio Liberman Bronfman <sergiolib@gmail.com>
2019-10-28 21:04:28 +0100

    IID sampling on pt pretraining

--- isw_pt_pretraining.py
+++ isw_pt_pretraining.py
@@ -5,6 +5,7 @@ from os import makedirs
 
 import numpy as np
 import tensorflow as tf
+import random
 
 from baseline_methods.pretraining import PretrainingBaseline
 from datasets.synth_datasets import gen_sine_data, gen_tasks
@@ -18,7 +19,7 @@ if len(gpus) > 0:
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("--learning_rate", nargs="+", type=float, default=[3e-6],
                              help="Learning rate")
-argument_parser.add_argument("--epochs", type=int, default=5000,
+argument_parser.add_argument("--epochs", type=int, default=10000,
                              help="Number of epochs to pre train for")
 argument_parser.add_argument("--n_tasks", type=int, default=400,
                              help="Number of tasks to pre train from")
@@ -32,10 +33,8 @@ argument_parser.add_argument("--save_models_every", type=int, default=100,
                              help="Amount of epochs to pass before saving models")
 argument_parser.add_argument("--check_val_every", type=int, default=100,
                              help="Amount of epochs to pass before checking validation loss")
-argument_parser.add_argument("--n_layers_rln", type=int, nargs="+", default=5,
-                             help="Amount of layers in the RLN")
-argument_parser.add_argument("--n_layers_tln", type=int, nargs="+", default=5,
-                             help="Amount of layers in the TLN")
+argument_parser.add_argument("--layers_rln", type=int, nargs="+", default=6,
+                             help="Amount of layers in the RLN and TLN")
 
 args = argument_parser.parse_args()
 
@@ -54,18 +53,22 @@ y_val = tf.convert_to_tensor(y_val, dtype=tf.float32)
 loss = float("inf")
 # Create logs directories
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-n_layers_tln = [args.n_layers_tln] if type(args.n_layers_tln) is int else args.n_layers_tln
-n_layers_rln = [args.n_layers_rln] if type(args.n_layers_rln) is int else args.n_layers_rln
-gen = product(n_layers_tln, n_layers_rln, args.learning_rate)
+layers_rln = args.layers_rln if type(args.layers_rln) is list else [args.layers_rln]
+gen = product(layers_rln, args.learning_rate)
+gen = list(gen)
+random.shuffle(gen)
 p = PretrainingBaseline(tf.keras.losses.MeanSquaredError())
-for tln_layers, rln_layers, lr in gen:
-    print(f"tln: {tln_layers}, rln: {rln_layers}, lr: {lr}")
-    train_log_dir = f'logs/pt_isw_lr{lr}_rln{rln_layers}_tln{tln_layers}/' + current_time + '/pre_train'
+for layers_rln, lr in gen:
+    layers_tln = 8 - layers_rln
+    print(f"rln: {layers_rln}, tln: {layers_tln}, lr: {lr}")
+    train_log_dir = f'logs/pt_isw_lr{lr}_rln{layers_rln}_tln{layers_tln}/' + current_time + '/pre_train'
     makedirs(train_log_dir, exist_ok=True)
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-    p.build_model(n_layers_rln=rln_layers, n_layers_tln=tln_layers, seed=0)
+    p.build_model(n_layers_rln=layers_rln, n_layers_tln=layers_tln, seed=0)
     val_loss_counts = 0
     previous_val_loss = p.compute_loss_no_training(x_val, y_val)
+    with train_summary_writer.as_default():
+        tf.summary.scalar("Validation Loss", previous_val_loss, step=0)
     val_loss = None
     for epoch in range(args.epochs):
         x_train, y_train, _, _ = gen_sine_data(tasks=train_tasks, n_functions=args.n_functions,
@@ -76,6 +79,11 @@ for tln_layers, rln_layers, lr in gen:
         x_train = np.vstack(x_train)
         y_train = np.vstack(y_train)
 
+        # According to figure 3, data comes IID
+        indices = np.random.permutation(len(x_train))
+        x_train = x_train[indices]
+        y_train = y_train[indices]
+
         # Numpy -> Tensorflow
         x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
         y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
@@ -83,24 +91,21 @@ for tln_layers, rln_layers, lr in gen:
         y_train = tf.reshape(y_train, (-1,))
 
         training_loss = float(p.pre_train(x_train, y_train, learning_rate=lr))
-        with train_summary_writer.as_default():
-            tf.summary.scalar("Training Loss", training_loss, step=epoch)
 
         if epoch % args.check_val_every == 0:
+            with train_summary_writer.as_default():
+                tf.summary.scalar("Training Loss", training_loss, step=epoch)
             val_loss = float(p.compute_loss_no_training(x_val, y_val))
             with train_summary_writer.as_default():
-                tf.summary.scalar("Validation Loss", val_loss, step=epoch)
+                tf.summary.scalar("Validation Loss", val_loss, step=epoch + 1)
 
             if previous_val_loss - val_loss < 1e-3:
                 val_loss_counts += 1
                 if val_loss_counts == 1:
-                    p.save_model(f"final_lr{lr}_rln{rln_layers}_tln{tln_layers}")
+                    p.save_model(f"final_lr{lr}_rln{layers_rln}_tln{layers_tln}")
                 elif val_loss_counts >= 6:
                     break
             else:
                 previous_val_loss = float(val_loss)
                 val_loss_counts = 0
 
-        # if epoch % args.save_models_every == 0:
-        #     p.save_model(f"{epoch}_lr{lr}_rln{rln_layers}_tln{tln_layers}")
-        #
