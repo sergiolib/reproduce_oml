fcce49c145ccb1d0ac93a42dbd9ef911ce784106
kosz <kosz@kth.se>
2019-10-28 16:10:02 +0100

    Move function to util

--- util/plotter.py
+++ util/plotter.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
+from datasets.synth_datasets import gen_tasks, gen_sine_data
 
 
 def visualize(representation):
@@ -12,3 +13,21 @@ def visualize(representation):
     plt.axis('off')
     plt.imshow(representation)
     plt.show()
+
+
+def plot_random_isw(n_fun_to_plot=2):
+    """
+    Plot some randomly generated incremental sine waves functions.
+    :param n_fun_to_plot: number of functions to plot. Max 10.
+    """
+    tasks = gen_tasks(10)
+    x_traj, y_traj, _, _ = gen_sine_data(tasks)
+
+    for i in range(n_fun_to_plot):
+        plt.scatter(x_traj[i][0][:][:, 0], y_traj[i][0][:])
+
+    plt.grid()
+    plt.show()
+
+
+plot_random_isw(n_fun_to_plot=5)
