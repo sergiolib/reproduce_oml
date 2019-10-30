f76b35841259deda81a62de5878c91dc4170c81c
mgst <mgst@kth.se>
2019-10-30 13:43:27 +0000

    Add changes to pretraining

--- omniglot_pt_pretraining.py
+++ omniglot_pt_pretraining.py
@@ -32,42 +32,45 @@ y_testing = tf.convert_to_tensor(y_testing)
 
 rln, tln = mrcl_omniglot()
 
-t = range(15000)
+t = range(1000)
 
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 train_log_dir = 'logs/classification/pretraining/omniglot/mrcl/gradient_tape/' + current_time + '/train'
 train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
-learning_rates = [0.3, 0.1, 0.03, 0.01, 0.003, 0.001, 0.0003, 0.0001]
+learning_rates = [0.1, 0.01, 0.001, 0.0001, 0.00001]
 for lr in learning_rates:
     train_log_dir = f'logs/omniglot_{lr}/' + current_time + '/pre_train'
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
     for epoch, v in enumerate(t):
         for x, y in tf.data.Dataset.from_tensor_slices((x_training, y_training)).shuffle(True).batch(256):
             loss, _ = pre_train(x, y, rln, tln, lr, pretraining_parameters)
-
+        print("learning rate:", lr, "Epoch:", epoch, "Training loss:", loss.numpy())
         with train_summary_writer.as_default():
             tf.summary.scalar('Training loss', loss, step=epoch)
+    
+        if epoch % 100 == 0 and epoch != 0:
+            total_correct = 0
+            for x, y in tf.data.Dataset.from_tensor_slices((x_training, y_training)).shuffle(True).batch(256):
+                loss, output = pre_train(x, y, rln, tln, lr, pretraining_parameters)
+                after_softmax = tf.nn.softmax(output, axis=1)
+                correct_prediction = tf.equal(tf.cast(tf.argmax(after_softmax, axis=1), tf.int32), y)
+                total_correct = total_correct + tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+            train_accuracy = total_correct / x_training.shape[0]
 
-        for x, y in tf.data.Dataset.from_tensor_slices((x_training, y_training)).shuffle(True).batch(256):
-            loss, output = pre_train(x, y, rln, tln, lr, pretraining_parameters)
-            after_softmax = tf.nn.softmax(output, axis=1)
-            correct_prediction = tf.equal(tf.cast(tf.argmax(after_softmax, axis=1), tf.int32), y)
-            total_correct = total_correct + tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
-        train_accuracy = total_correct / x_training.shape[0]
+            total_correct = 0
+            for x, y in tf.data.Dataset.from_tensor_slices((x_testing, y_testing)).shuffle(True).batch(256):
+                loss, output = pre_train(x, y, rln, tln, lr, pretraining_parameters)
+                after_softmax = tf.nn.softmax(output, axis=1)
+                correct_prediction = tf.equal(tf.cast(tf.argmax(after_softmax, axis=1), tf.int32), y)
+                total_correct = total_correct + tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+            test_accuracy = total_correct / x_testing.shape[0]
 
-        for x, y in tf.data.Dataset.from_tensor_slices((x_testing, y_testing)).shuffle(True).batch(256):
-            loss, output = pre_train(x, y, rln, tln, lr, pretraining_parameters)
-            after_softmax = tf.nn.softmax(output, axis=1)
-            correct_prediction = tf.equal(tf.cast(tf.argmax(after_softmax, axis=1), tf.int32), y)
-            total_correct = total_correct + tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
-        test_accuracy = total_correct / x_testing.shape[0]
+            with train_summary_writer.as_default():
+                tf.summary.scalar('Training accuracy', train_accuracy, step=epoch)
+                tf.summary.scalar('Testing accuracy', test_accuracy, step=epoch)
 
-        with train_summary_writer.as_default():
-            tf.summary.scalar('Training accuracy', train_accuracy, step=epoch)
-            tf.summary.scalar('Testing accuracy', test_accuracy, step=epoch)
-
-        print("Epoch:", epoch, "Training loss:", loss.numpy(), "Training accuracy:", train_accuracy, "Testing accuracy:", test_accuracy)
+            print("Epoch:", epoch, "Training loss:", loss.numpy(), "Training accuracy:", train_accuracy.numpy(), "Testing accuracy:", test_accuracy.numpy())
         if (epoch+1) % 1000 == 0:
             save_models(tln, f"tln_basic_pretraining_{epoch}_{lr}_omniglot")
             save_models(rln, f"rln_basic_pretraining_{epoch}_{lr}_omniglot")
