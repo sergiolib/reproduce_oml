021d2836a40a6db4fb80843769f0017b17e20326
Mihaela Stoycheva <mihaela.stoycheva@gmail.com>
2019-10-30 10:46:26 +0100

    Remove he initialization

--- experiments/exp4_2/omniglot_model.py
+++ experiments/exp4_2/omniglot_model.py
@@ -8,7 +8,7 @@ from experiments.training import copy_parameters
 def mrcl_omniglot_rln(inputs, n_layers, filters, strides=[2, 1, 2, 1, 2, 2]):
     h = inputs
     for i in range(n_layers):
-        h = tf.keras.layers.Conv2D(filters, (3, 3), activation='relu', input_shape=(84, 84, 1), strides=strides[i], kernel_initializer="he_normal")(h)
+        h = tf.keras.layers.Conv2D(filters, (3, 3), activation='relu', input_shape=(84, 84, 1), strides=strides[i])(h)
     h = tf.keras.layers.Flatten()(h)
     return h
 
@@ -16,8 +16,8 @@ def mrcl_omniglot_rln(inputs, n_layers, filters, strides=[2, 1, 2, 1, 2, 2]):
 def mrcl_omniglot_tln(inputs, n_layers, hidden_units_per_layer, output=964):
     y = inputs
     for i in range(n_layers - 1):
-        y = tf.keras.layers.Dense(hidden_units_per_layer, activation='relu', kernel_initializer="he_normal")(y)
-    y = tf.keras.layers.Dense(output, kernel_initializer="he_normal")(y)
+        y = tf.keras.layers.Dense(hidden_units_per_layer, activation='relu')(y)
+    y = tf.keras.layers.Dense(output)(y)
     return y
 
 
