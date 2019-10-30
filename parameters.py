c9b58cc43b3d2182d572d81f7f0e43f4e5161e05
kosz <kosz@kth.se>
2019-10-30 19:13:00 +0100

    Remove default setting to limit GPU memory

--- parameters.py
+++ parameters.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 num_gb_to_use = 8
+limit_gpu = False
 
-if tf.test.is_gpu_available():
+if tf.test.is_gpu_available() and limit_gpu:
     gpus = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_virtual_device_configuration(gpus[0], [
         tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(1024 * num_gb_to_use))])
