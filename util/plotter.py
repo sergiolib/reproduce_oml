8badbc967e1ba0f8178d640ba3361aa2ce870bb4
Mihaela Stoycheva <mihaela.stoycheva@gmail.com>
2019-10-31 22:40:23 +0100

    Add normalisation and saving capabilities for visualization of representation

--- util/plotter.py
+++ util/plotter.py
@@ -1,23 +1,29 @@
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from datasets.synth_datasets import gen_tasks, gen_sine_data
 
 
-def visualize(representation):
+def visualize(representation, save_name=None):
     """
     Visualize the representation (weights) of a layer for omniglot (fig.5 in paper)
     :param representation: last layer's weights of RLN as numpy array
     """
     representation = representation.reshape((32, 72))
-    representation = normalize(representation)
+    scaler = MinMaxScaler()
+    scaler = scaler.fit(representation)
+    representation = scaler.transform(representation)
     plt.axis('off')
     pos = plt.imshow(representation, cmap="YlGn")
 
     divider = make_axes_locatable(plt.gca())
     cax = divider.append_axes("right", size="5%", pad=0.05)
     plt.gcf().colorbar(pos, cax=cax)
+    if save_name is not None:
+        plt.savefig(save_name, bbox_inches='tight')
     plt.show()
+
+
 
 
 def plot_random_isw(n_fun_to_plot=2):
